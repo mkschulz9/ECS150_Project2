5365257904eed457830d2204b112cfbf1d47be86
tpandrews <115209083+tpandrews@users.noreply.github.com>
2022-11-23 09:58:34 -0800

    Update fs.c
    Phase 2 & 3

--- libfs/fs.c
+++ libfs/fs.c
@@ -10,7 +10,7 @@
 #define signature_default "ECS150FS"
 
 struct superblock {
-	uint64_t sig;                 // must be equal to "ECS150FS"
+	char sig[10];                 // must be equal to "ECS150FS"
 	uint16_t diskBlk_total;       // total amount of blocks of virtual disk
 	uint16_t rootBlk_index;       // root directory block index
 	uint16_t firstDataBlk_index;  // data block start index
@@ -28,22 +28,30 @@ struct file {
 	uint32_t file_size;           // size of file (in bytes)
 	uint16_t index_DataBlk;       // index of first data block
 	uint8_t pad[10];              // unused, padding
+	size_t offset;
 
 };
 
+struct openFiles {
+	int openCount;
+	struct file file[FS_OPEN_MAX_COUNT]
+}
+
 struct root_dir {
-	struct file root_dir[FS_OPEN_MAX_COUNT];
+	struct file root_dir[FS_FILE_MAX_COUNT];
 };
 
 // define struct objs needed in functions below
 struct superblock superblock;
 struct fat fat;
 struct root_dir root_dir;
+struct openFiles openFiles;
 
 // create empty structs to use in unmount
 const struct superblock empty_superblock;
 const struct fat empty_fat;
 const struct root_dir empty_root_dir;
+const struct openFiles empty_openFiles;
 
 int fs_mount(const char *diskname)
 {
@@ -93,6 +101,7 @@ int fs_umount(void)
 	superblock = empty_superblock;
 	fat = empty_fat;
 	root_dir = empty_root_dir;
+	openFiles = empty_openFiles;
 
 	return 0;
 }
@@ -104,37 +113,153 @@ int fs_info(void)
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
+	// Check validity of filename
+	if (!filename || FS_FILENAME_LEN < strlen(filename)) return -1;
+
+	// Check number of files in directory
+	int count = 0, i = 0;
+
+	while (i < FS_FILE_MAX_COUNT) {
+		if(strlen(root_dir.root_dir[i].filename) != 0) {
+			if(!strcmp(root_dir.root_dir[i].filename, filename)) return -1;
+		} else {
+			count = count + 1;
+		}
+		i = i + 1;
+	}
+
+	// Max number of files reached
+	if (count >= FS_FILE_MAX_COUNT) return -1;
+
+	i = 0;
+	while (i < FS_FILE_MAX_COUNT) {
+		// Initializing empty file with filename
+		if(strlen(root_dir.root_dir[i].filename) == 0) {
+			strcpy(root_dir.root_dir[i].filename, filename);
+			root_dir.root_dir[i].file_size = 0;
+			root_dir.root_dir[i].index_DataBlk = FAT_EOC;
+			return 0;
+		}
+		i = i + 1;
+	}
+	return 0;
 }
 
 int fs_delete(const char *filename)
 {
-	/* TODO: Phase 2 */
+	// Check validity of filename
+	if (!filename || FS_FILENAME_LEN < strlen(filename)) return -1;
+
+	// Use check to test whether file is found or not
+	int check = 1, i = 0;
+
+	uint16_t blockIndex = FAT_EOC;
+
+	while (i < FS_FILE_MAX_COUNT) {
+		if(!strcmp(root_dir.root_dir[i].filename, filename)) {
+			check = 0;
+			blockIndex = root_dir.root_dir[i].index_DataBlk;
+			strcpy(root_dir.root_dir[i].filename, "\0");
+			root_dir.root_dir[i].file_size = 0;
+			root_dir.root_dir[i].index_DataBlk = FAT_EOC;
+
+			if(block_write(superblock.rootBlk_index, &root_dir.root_dir) == -1) return -1;
+			break;
+		}
+		i = i + 1;
+	}
+
+	if (check) return -1;
+
+	while (blockIndex != FAT_EOC) {
+		uint16_t newIndex = fat.entries[blockIndex];
+		fat.arr[blockIndex] = 0;
+		blockIndex = newIndex;
+	}
+
+	return 0;
 }
 
 int fs_ls(void)
 {
-	/* TODO: Phase 2 */
+	printf("FS Ls:")
+	int i = 0;
+	while (i < FS_FILE_MAX_COUNT) {
+		if(!strlen(root_dir.root_dir[0])) {
+			printf("\nfile: %s, size: %i, data_blk: %i", root_dir.root_dir[i].filename, root_dir.root_dir[i].file_size, root_dir.root_dir[i].index_DataBlk);
+		}
+		i = i + 1;
+	}
+	return 0;
 }
 
 int fs_open(const char *filename)
 {
-	/* TODO: Phase 3 */
+	if (!filename || FS_FILENAME_LEN < strlen(filename)) return -1;
+
+	int check = 1, i = 0;
+
+	while (i < FS_FILE_MAX_COUNT) {
+		if (!strcmp(root_dir.root_dir[i].filename,filename))
+			check = 0;
+		i = i + 1;
+	}
+
+	if (check) return -1;
+
+	if (openFiles.openCount >= FS_OPEN_MAX_COUNT) return -1;
+
+	int fd = -1;
+	i = 0;
+
+	while (i < FS_OPEN_MAX_COUNT) {
+		if (!strlen(openFiles.file[i].filename)) {
+			openFiles.openCount = openFiles.openCount + 1;
+			strcpy(openFiles.file[i].filename, filename);
+			openFiles.file[i].offset = 0;
+			fd = i;
+			break;
+		}
+
+		i = i + 1;
+	}
+
+	return fd;
 }
 
 int fs_close(int fd)
 {
-	/* TODO: Phase 3 */
+	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
+	strcpy(openFiles.file[fd].filename, "\0");
+	openFiles.file[fd].offset = 0;
+	openFiles.openCount = openFiles.openCount - 1;
+	return 0;
 }
 
 int fs_stat(int fd)
 {
-	/* TODO: Phase 3 */
+	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
+
+	int size = -1, i = 0;
+
+	while (i < FS_FILE_MAX_COUNT) {
+		if(!strcmp(root_dir.root_dir[i].filename, filename)) {
+			size = root_dir.root_dir[i].file_size;
+		}
+		i = i + 1;
+	}
+
+	return size;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
-	/* TODO: Phase 3 */
+	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
+	if (offset < 0 || offset > fs_stat(fd)) return -1;
+	if (fs_stat(fd) == -1) return -1;
+
+	openFiles.file[fd].offset = offset;
+	return 0;
 }
 
 int fs_write(int fd, void *buf, size_t count)
