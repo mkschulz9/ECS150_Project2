dc1b9fa9bac33409e39efb9ab6e68e34db42a000
tpandrews <115209083+tpandrews@users.noreply.github.com>
2022-11-23 16:58:12 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,10 +7,12 @@
 #include "disk.h"
 #include "fs.h"
 
+#define FAT_EOC 0xFFFF
+
 #define signature_default "ECS150FS"
 
 struct superblock {
-	char sig[10];                 // must be equal to "ECS150FS"
+	char sig[8];                 // must be equal to "ECS150FS"
 	uint16_t diskBlk_total;       // total amount of blocks of virtual disk
 	uint16_t rootBlk_index;       // root directory block index
 	uint16_t firstDataBlk_index;  // data block start index
@@ -20,22 +22,25 @@ struct superblock {
 };
 
 struct fat {
-	uint16_t entries[4 * (BLOCK_SIZE/2)];
+	uint16_t *entries;
 };
 
 struct file {
-	uint8_t filename[FS_FILENAME_LEN];         // filename (including NULL char)
+	char filename[FS_FILENAME_LEN];         // filename (including NULL char)
 	uint32_t file_size;           // size of file (in bytes)
 	uint16_t index_DataBlk;       // index of first data block
 	uint8_t pad[10];              // unused, padding
-	size_t offset;
+};
 
+struct openEntry {
+	char filename[FS_FILENAME_LEN];
+	uint16_t offset;
 };
 
 struct openFiles {
 	int openCount;
-	struct file file[FS_OPEN_MAX_COUNT]
-}
+	struct openEntry entry[FS_OPEN_MAX_COUNT];
+};
 
 struct root_dir {
 	struct file root_dir[FS_FILE_MAX_COUNT];
@@ -60,27 +65,42 @@ int fs_mount(const char *diskname)
 
 	// return -1 if superblock can't be read
 	if (block_read(0, &superblock) == -1) return -1;
-
-	// return -1 if root directory can't be read
-	if (block_read(superblock.rootBlk_index, &root_dir.root_dir) == -1) return -1;
-
-	// read over fat entries
-	for (int i = 1; i < superblock.num_fatBlks; i++) {
-		// return -1 if cant read fat entry
-		if (block_read(i, &fat.entries[i * (BLOCK_SIZE/2)]) == -1) 		return -1;
-	}
+	
+	// return -1 if total amount of blocks doesn;t correspond to what block_disk_count() returns
+	if(superblock.diskBlk_total != block_disk_count()) return -1;
 
 	// return -1 if signature doesn't match specifications
-	if (superblock.sig != signature_default) return -1; // double check this part
+	if (memcmp(superblock.sig, signature_default, 8) != 0) return -1; // double check this part
 
-	// return -1 if total amount of blocks doesn;t correspond to what  block_disk_count() returns
-	if (superblock.num_datablks != block_disk_count()) return -1;
+	fat.entries = (uint16_t*)malloc(superblock.num_datablks * sizeof(uint16_t));
+
+	void *buf = (void*)malloc(BLOCK_SIZE);
+	int i = 1;
+	// read over fat entries
+	while (i < superblock.rootBlk_index) {
+		// return -1 if cant read fat entry
+		if(block_read(i, buf) == -1) return -1;
+		memcpy(fat.entries + (i-1)*BLOCK_SIZE, buf, BLOCK_SIZE);
+		i = i + 1;
+	}
+
+	if(fat.entries[0] != FAT_EOC) return -1;
+
+	// return -1 if root directory can't be read
+	if (block_read(superblock.rootBlk_index, &root_dir) == -1) return -1;
 
 	return 0;
 }
 
 int fs_umount(void)
 {
+
+	// empty out structs
+	superblock = empty_superblock;
+	fat = empty_fat;
+	root_dir = empty_root_dir;
+	openFiles = empty_openFiles;
+
 	// return -1 if no FS is currently mounted
 	if ((block_disk_count()) == -1) return -1;
 
@@ -89,19 +109,13 @@ int fs_umount(void)
 
 	// write fat entries to disk
 	for (int i = 1; i < superblock.num_fatBlks; i++) {
-		if (block_write(i, &(fat.entries[i * (BLOCK_SIZE/2)])) == -1) 		return -1;
+		if (block_write(i, fat.entries + (i-1) * BLOCK_SIZE) == -1) 		return -1;
 	}
 
 	//return 0 if VFS cannot be closed
 	if (block_disk_close() == -1) return -1;
 
 	//check if there are still open FDs (ToDo Phase 3)
-
-	// empty out structs
-	superblock = empty_superblock;
-	fat = empty_fat;
-	root_dir = empty_root_dir;
-	openFiles = empty_openFiles;
 
 	return 0;
 }
@@ -152,10 +166,9 @@ int fs_create(const char *filename)
 
 	while (i < FS_FILE_MAX_COUNT) {
 		if(strlen(root_dir.root_dir[i].filename) != 0) {
-			if(!strcmp(root_dir.root_dir[i].filename, filename)) return -1;
-		} else {
 			count = count + 1;
-		}
+		} 
+		if(strcmp(root_dir.root_dir[i].filename, filename) == 0) return -1;
 		i = i + 1;
 	}
 
@@ -169,7 +182,7 @@ int fs_create(const char *filename)
 			strcpy(root_dir.root_dir[i].filename, filename);
 			root_dir.root_dir[i].file_size = 0;
 			root_dir.root_dir[i].index_DataBlk = FAT_EOC;
-			return 0;
+			break;
 		}
 		i = i + 1;
 	}
@@ -187,14 +200,14 @@ int fs_delete(const char *filename)
 	uint16_t blockIndex = FAT_EOC;
 
 	while (i < FS_FILE_MAX_COUNT) {
-		if(!strcmp(root_dir.root_dir[i].filename, filename)) {
+		if(strcmp(root_dir.root_dir[i].filename, filename) == 0) {
 			check = 0;
 			blockIndex = root_dir.root_dir[i].index_DataBlk;
 			strcpy(root_dir.root_dir[i].filename, "\0");
 			root_dir.root_dir[i].file_size = 0;
 			root_dir.root_dir[i].index_DataBlk = FAT_EOC;
 
-			if(block_write(superblock.rootBlk_index, &root_dir.root_dir) == -1) return -1;
+			if(block_write(superblock.rootBlk_index, &root_dir) == -1) return -1;
 			break;
 		}
 		i = i + 1;
@@ -204,7 +217,7 @@ int fs_delete(const char *filename)
 
 	while (blockIndex != FAT_EOC) {
 		uint16_t newIndex = fat.entries[blockIndex];
-		fat.arr[blockIndex] = 0;
+		fat.entries[blockIndex] = 0;
 		blockIndex = newIndex;
 	}
 
@@ -213,10 +226,10 @@ int fs_delete(const char *filename)
 
 int fs_ls(void)
 {
-	printf("FS Ls:")
+	printf("FS Ls:");
 	int i = 0;
 	while (i < FS_FILE_MAX_COUNT) {
-		if(!strlen(root_dir.root_dir[0])) {
+		if(strlen(root_dir.root_dir[0].filename) != 0) {
 			printf("\nfile: %s, size: %i, data_blk: %i", root_dir.root_dir[i].filename, root_dir.root_dir[i].file_size, root_dir.root_dir[i].index_DataBlk);
 		}
 		i = i + 1;
@@ -231,8 +244,7 @@ int fs_open(const char *filename)
 	int check = 1, i = 0;
 
 	while (i < FS_FILE_MAX_COUNT) {
-		if (!strcmp(root_dir.root_dir[i].filename,filename))
-			check = 0;
+		if (strcmp(root_dir.root_dir[i].filename,filename) == 0) check = 0;
 		i = i + 1;
 	}
 
@@ -244,10 +256,10 @@ int fs_open(const char *filename)
 	i = 0;
 
 	while (i < FS_OPEN_MAX_COUNT) {
-		if (!strlen(openFiles.file[i].filename)) {
+		if (strlen(openFiles.entry[i].filename) == 0) {
 			openFiles.openCount = openFiles.openCount + 1;
-			strcpy(openFiles.file[i].filename, filename);
-			openFiles.file[i].offset = 0;
+			strcpy(openFiles.entry[i].filename, filename);
+			openFiles.entry[i].offset = 0;
 			fd = i;
 			break;
 		}
@@ -260,21 +272,21 @@ int fs_open(const char *filename)
 
 int fs_close(int fd)
 {
-	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
-	strcpy(openFiles.file[fd].filename, "\0");
-	openFiles.file[fd].offset = 0;
+	if (fd > 31 || fd < 0 || strlen(openFiles.entry[fd].filename) == 0) return -1;
+	strcpy(openFiles.entry[fd].filename, "\0");
+	openFiles.entry[fd].offset = 0;
 	openFiles.openCount = openFiles.openCount - 1;
 	return 0;
 }
 
 int fs_stat(int fd)
 {
-	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
+	if (fd > 31 || fd < 0 || !strlen(openFiles.entry[fd].filename)) return -1;
 
 	int size = -1, i = 0;
 
 	while (i < FS_FILE_MAX_COUNT) {
-		if(!strcmp(root_dir.root_dir[i].filename, filename)) {
+		if(strcmp(root_dir.root_dir[i].filename, openFiles.entry[fd].filename) == 0) {
 			size = root_dir.root_dir[i].file_size;
 		}
 		i = i + 1;
@@ -285,13 +297,19 @@ int fs_stat(int fd)
 
 int fs_lseek(int fd, size_t offset)
 {
-	if (fd > 31 || fd < 0 || !strlen(openFiles.file[fd].filename)) return -1;
+	if (fd > 31 || fd < 0 || !strlen(openFiles.entry[fd].filename)) return -1;
 	if (offset < 0 || offset > fs_stat(fd)) return -1;
 	if (fs_stat(fd) == -1) return -1;
 
-	openFiles.file[fd].offset = offset;
+	openFiles.entry[fd].offset = offset;
 	return 0;
 }
+
+int fs_write(int fd, void *buf, size_t count)
+{
+	/* TODO: Phase 4 */
+}
+
 
 int fs_write(int fd, void *buf, size_t count)
 {
